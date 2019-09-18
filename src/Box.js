7c83b44b222f84138eda620e748625046ae21525
matthewjordanau@gmail.com <matthewjordanau@gmail.com>
2019-09-17 21:20:02 -0700

    started robot build

--- src/Box.js
+++ src/Box.js
@@ -5,11 +5,11 @@ export default class Box extends Component {
   
 
   render(){
-    const { value } = this.props
+    const { piece } = this.props
     return(
       <div>
         <div id="box">
-          { value }
+          { piece }
         </div>
       </div>
     )
--- src/Grid.js
+++ src/Grid.js
@@ -14,10 +14,11 @@ export default class Grid extends Component {
          0, 0, 0, 0, 0],
       robotIndex: 0,
       batteryIndex: 0,
+      success: false,
     }
   }
 
-  handleStartButton = () => {
+  handleStart = () => {
     const { board, robotIndex, batteryIndex } = this.state
     if (batteryIndex === 0 && robotIndex === 0) {
       let robotPosition = Math.floor(Math.random() * 25)
@@ -43,17 +44,56 @@ export default class Grid extends Component {
     }
   }
 
-  handleResetButton = () => {
+  handleReset = () => {
     const { robotIndex, batteryIndex } = this.state
-    
+    if (robotIndex !== 0 || batteryIndex !== 0) {
+      this.setState({
+        board:
+        [0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0],
+      robotIndex: 0,
+      batteryIndex: 0,
+      success: false,
+      })
+    }
+  }
+
+  //Check functionality after completing win condition
+  handleNextGame = () => {
+    const { success } = this.state
+    if (success) {
+      this.handleStart()
+    }
   }
 
   render(){
     const { board } = this.state
     const grid = board.map((value, index) => {
+      let piece
+      if (value === 1) {
+        piece = 
+          <div id="robot">
+            <div id="antenna"></div>
+            <div id="head">
+              <div id="leftEye"></div>
+              <div id="rightEye"></div>
+              <div id="mouth"></div>
+            </div>
+            <div id="torso"></div>
+            <div id="leftArm"></div>
+            <div id="rightArm"></div>
+            <div id="foot"></div>
+          </div>
+      } else if (value === 2) {
+        piece =
+          <img src={require("./battery.png")} alt="battery" className="img-responsive"/>
+      }
       return(
         <Box
-        value={ value }
+        piece={ piece }
         index={ index }
         handleClick={ this.handleClick }
         board={ board }
@@ -69,9 +109,9 @@ export default class Grid extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md">
-              <button type="button" className="btn btn-primary" onClick={ this.handleStartButton } id="startButton">Start</button>
-              <button type="button" className="btn btn-danger" id="resetButton">Reset</button>
-              <button type="button" className="btn btn-success" id="nextGameButton">Next Game</button>
+              <button type="button" className="btn btn-primary" onClick={ this.handleStart } id="startButton">Start</button>
+              <button type="button" className="btn btn-danger" onClick={ this.handleReset } id="resetButton">Reset</button>
+              <button type="button" className="btn btn-success" onClick={ this.handleNextGame } id="nextGameButton">Next Game</button>
             </div>
           </div>
         </div>
